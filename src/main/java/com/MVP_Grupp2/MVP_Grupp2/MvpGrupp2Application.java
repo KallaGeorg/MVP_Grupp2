1a16a9d89413dbe168f8efe4641ae4b4156f4087
KallaGeorg <kalla.georg@yahoo.com>
2024-03-01 10:56:48 +0100

    first commit

---
+++ src/main/java/com/MVP_Grupp2/MVP_Grupp2/MvpGrupp2Application.java
@@ -0,0 +1,13 @@
+package com.MVP_Grupp2.MVP_Grupp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpGrupp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvpGrupp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/MVP_Grupp2/MVP_Grupp2/MvpGrupp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MVP_Grupp2.MVP_Grupp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpGrupp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
