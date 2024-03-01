4ba79bc324cfbba5d654ec9976f7b7c8071c979b
KallaGeorg <kalla.georg@yahoo.com>
2024-03-01 11:07:15 +0100

    #2 Sätta upp ramverket

--- src/main/java/com/MVP_Grupp2/MVP_Grupp2/Service/MvpGrupp2Application.java
+++ src/main/java/com/MVP_Grupp2/MVP_Grupp2/Service/MvpGrupp2Application.java
@@ -1,4 +1,4 @@
-package com.MVP_Grupp2.MVP_Grupp2;
+package com.MVP_Grupp2.MVP_Grupp2.Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
