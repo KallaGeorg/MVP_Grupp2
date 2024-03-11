632dad484a52a8172a9adeffe093dbdb10ed7ea8
samsam1992 <143456029+Samback92@users.noreply.github.com>
2024-03-11 12:02:15 +0100

    Update MockService.java

--- src/main/java/com/MVP_Grupp2/MVP_Grupp2/Service/MockService.java
+++ src/main/java/com/MVP_Grupp2/MVP_Grupp2/Service/MockService.java
@@ -26,61 +26,155 @@ import com.MVP_Grupp2.MVP_Grupp2.Repository.TopRepository;
 
 @Service
 public class MockService {
-    
-   
-  //  private final CustomerRepository customerRepository;
+  /* 
+  private CustomerRepository customerRepository;
 
-  //  private final OrderRepository orderRepository;
-   
-   
-  
-  //   @Autowired
-  //   public MockService(CustomerRepository customerRepository, OrderRepository orderRepository) {
-  //       this.orderRepository = orderRepository;
-  //       this.customerRepository = customerRepository;
-  //       int order_number = generateOrderNumber();
-  //       CustomerOrder order = new CustomerOrder();
-  //       order.setOrder_number(order_number);
-  //       order.setOrderDate(new Date());
-  //       order.setOrderSumma(100);
-  //       order.setPayment("Credit Card"); 
-  //       order.setStatus("Pending");
-       
-  //     Customer customer = new Customer();
-  //       customer.setName("John Doe");
-  //       customer.setAdress("123 Main St");
-  //       customer.setEmail("john@example.com");
-  //       customer.setPassword("password123");
-  //       customer.setPayment("Credit Card");
-  //     customer.setOrder(order);
+  private OrderRepository orderRepository;  */
       
-        
-  //       saveOrder(order, customer);
+  @Autowired
+  private BottomRepository bottomRepository;
+  private TopRepository topRepository;
+  private ShoesRepository shoesRepository;
 
-  //   }
-  //   @Transactional
-  //   public void saveOrder(CustomerOrder order, Customer customer) {
-  //       Customer savedCustomer = customerRepository.save(customer);
-  //       UUID customerNumber = savedCustomer.getCustomerNumber();
-  //       order.setCustomerNumber(customerNumber);
-  //       orderRepository.save(order);
-  //   }
-    
-  //   private int generateOrderNumber() {
-  //     // Generate a timestamp-based order number
-  //     long currentTime = System.currentTimeMillis();
-  //     // Append a random number to the timestamp to ensure uniqueness
-  //     Random random = new Random();
-  //     int randomNumber = random.nextInt(100000); // Generate a random number with 5 digits
-  //     // Concatenate timestamp and random number, ensuring it doesn't exceed the maximum integer value
-  //     long concatenatedNumber = Long.parseLong(String.valueOf(currentTime % 1000000000) + String.format("%05d", randomNumber));
-  //     // Truncate the concatenated number to fit within the integer range
-  //     return (int) concatenatedNumber;
-  // }
-   
-    
+  public MockService(BottomRepository bottomRepository, TopRepository topRepository, ShoesRepository shoesRepository) {
+    /*  this.orderRepository = orderRepository;
+    this.customerRepository = customerRepository;*/
+    this.bottomRepository = bottomRepository;
+    this.topRepository = topRepository;
+    this.shoesRepository = shoesRepository;
         
+    /* int order_number = generateOrderNumber();
+    CustomerOrder order = new CustomerOrder();
+    order.setOrder_number(order_number);
+    order.setOrderDate(new Date());
+    order.setOrderSumma(100);
+    order.setPayment("Credit Card"); 
+    order.setStatus("Pending");
+        
+    Customer customer = new Customer();
+    customer.setName("John Doe");
+    customer.setAdress("123 Main St");
+    customer.setEmail("john@example.com");
+    customer.setPassword("password123");
+    customer.setPayment("Credit Card");
+    customer.setOrder(order);
+              
+    
+    saveOrder(order, customer); */
+    createBottoms();
+    createTops();
+    createShoes();
+
+  }
+
+  private void createBottoms() {
+    Bottom herrbyxor = new Bottom(
+      1, // articleNumber
+      "Herr", // genderBottom
+      "500 SEK", // priceBottom
+      "Herrbyxor", // nameBottom
+      null, // sizeBottom
+      40, // saldoBottom
+      null, // ratingBottom
+      null // commentBottom
+    );
+    Bottom dambyxor = new Bottom(
+      2,
+      "Dam",
+      "600 SEK",
+      "Dambyxor",
+      null,
+      80,
+      null,
+      null
+    );
+
+    bottomRepository.save(herrbyxor);
+    bottomRepository.save(dambyxor);
+  }
+
+  private void createTops() {
+    Top herrtop = new Top(
+      11, 
+      "Herr", 
+      "200", 
+      "T-shirt Herr", 
+      null, 
+      150, 
+      null, 
+      null
+      );
+    Top damtop = new Top(
+      12, 
+      "Dam", 
+      "200", 
+      "T-shirt Dam", 
+      null, 
+      100, 
+      null, 
+      null
+    );
+    
+    topRepository.save(herrtop);
+    topRepository.save(damtop);
+
+  }
+
+
+  private void createShoes() {
+    Shoes herrshoes = new Shoes(
+      21, 
+      "Herr", 
+      "800", 
+      "Herrskor", 
+      null, 
+      60, 
+      null, 
+      null
+    );
+    Shoes damshoes = new Shoes(
+      22, 
+      "Dam", 
+      "600", 
+      "Damskor", 
+      null, 
+      60, 
+      null, 
+      null
+    );
+
+    shoesRepository.save(herrshoes);
+    shoesRepository.save(damshoes);
+
+  }
+
+
+
+
+
+
+  /*
+  @Transactional
+    public void saveOrder(CustomerOrder order, Customer customer) {
+      Customer savedCustomer = customerRepository.save(customer);
+      UUID customerNumber = savedCustomer.getCustomerNumber();
+      order.setCustomerNumber(customerNumber);
+      orderRepository.save(order);
     }
+
+    private int generateOrderNumber() {
+      // Generate a timestamp-based order number
+      long currentTime = System.currentTimeMillis();
+      // Append a random number to the timestamp to ensure uniqueness
+      Random random = new Random();
+      int randomNumber = random.nextInt(100000); // Generate a random number with 5 digits
+      // Concatenate timestamp and random number, ensuring it doesn't exceed the maximum integer value
+      long concatenatedNumber = Long.parseLong(String.valueOf(currentTime % 1000000000) + String.format("%05d", randomNumber));
+      // Truncate the concatenated number to fit within the integer range
+      return (int) concatenatedNumber;
+    }  */
+
+}
 
 
 
