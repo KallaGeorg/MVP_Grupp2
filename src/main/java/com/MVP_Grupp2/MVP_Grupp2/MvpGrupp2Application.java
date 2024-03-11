0e7f661369d8b87fbda007ac4b8dc985f5c9ba79
dennis <isacsson_89@hotmail.com>
2024-03-11 20:34:25 +0100

    ka registrera användare utan vidarekoppling

--- src/main/java/com/MVP_Grupp2/MVP_Grupp2/MvpGrupp2Application.java
+++ src/main/java/com/MVP_Grupp2/MVP_Grupp2/MvpGrupp2Application.java
@@ -2,6 +2,9 @@ package com.MVP_Grupp2.MVP_Grupp2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MvpGrupp2Application {
@@ -9,5 +12,16 @@ public class MvpGrupp2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(MvpGrupp2Application.class, args);
 	}
-
+	    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+	}
 }
